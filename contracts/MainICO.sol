0bbb967c4dcd7cd1d3408383e368a754a2538972
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-04-03 14:20:45 +0500

    small changes

--- contracts/MainICO.sol
+++ contracts/MainICO.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.11;
 
 import "./BaseICO.sol";
 import "./Stage1ICO.sol";
@@ -26,22 +26,45 @@ contract MainICO is BaseICO {
 
     function _buy_price() internal returns(uint256) {
         Log("[Main ICO] _buy_price");
+        return getContract().buyPrice();
+    }
+
+    function getContract() internal returns(BaseICO) {
         if (now > startTime + 4 minutes){
             //третий	вариант
             if (!_contract.isName("Stage3ICO")) {
                 _contract = new Stage3ICO(token);
             }
-            return _contract.buyPrice();
+            emit Log("stage 3");
+            return _contract;
         }
         if (now > startTime + 2 minutes)	{
             //	второй	вариант
             if  (!_contract.isName("Stage2ICO")) {
                 _contract = new Stage2ICO(token);
             }
-            return _contract.buyPrice();
+            Log("stage 2");
+            return _contract;
         }
         // первый вариант
-        return _contract.buyPrice();
+        Log("stage 1");
+        return _contract;
+    }
+
+    function getCurrentTime() returns (uint){
+        return now;
+    }
+
+    function _now() view public returns(uint) {
+        return now;
+    }
+
+    function stage() view public returns(string) {
+        return bytes32ToString(getContract().name());
+    }
+
+    function buy_price() view public returns(uint) {
+        return _buy_price();
     }
 
 }
